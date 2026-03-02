d442cef90024809e21a855763701f3640bc562be
Carlos Bazaga <cabazaga@gmail.com>
2026-03-02 06:59:34 +0100

    Update draw function to use Gemini model and adjust image generation settings

--- game/illustrator/illustrator_gemini.py
+++ game/illustrator/illustrator_gemini.py
@@ -11,7 +11,7 @@ from PIL import Image
 load_dotenv(override=True)
 
 # Define globals.
-MODEL = 'gemini-2.5-flash-image-preview'
+MODEL = 'gemini-2.5-flash-image'
 
 # Client instantiation.
 CLIENT = genai.Client()
